6a981fa6e993dd1128c6bcec3689a8d8249575b0
Taranvir Singh Saini <taranvir.554@gmail.com>
2024-11-02 21:58:52 +0000

    updated sidebar logout button

--- UI/upload_app.py
+++ UI/upload_app.py
@@ -85,7 +85,7 @@ class Streamlit_Upload_App:
         st.sidebar.title(f"User:")
         st.sidebar.text(f"{repr(self.decoded_token['email'])}")
 
-        if st.button("Logout"):
+        if st.sidebar.button("Logout"):
             for key in list(st.session_state.keys()):
                 del st.session_state[key]
             st.experimental_rerun()  # Refresh the app to reflect logout
